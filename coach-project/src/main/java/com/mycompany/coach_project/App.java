6b161ed56623340352180cdd666a0146df371e59
Nilay Das <mailnilaydas@gmail.com>
2019-09-25 15:58:38 +0530

    initial commit

---
+++ coach-project/src/main/java/com/mycompany/coach_project/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.coach_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
